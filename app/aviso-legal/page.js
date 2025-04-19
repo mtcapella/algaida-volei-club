de6fe62767097491977ddcaba67b192c412940fe
mtcapella <miqueltoni@gmail.com>
2025-04-19 22:36:32 +0200

    css fixes

---
+++ app/aviso-legal/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useTranslation } from "react-i18next";
+
+export default function AvisoLegal() {
+  const { t } = useTranslation();
+
+  return (
+    <section style={{ marginBottom: "1.5rem" }}>
+      <h2> Aviso </h2>
+      <p style={{ whiteSpace: "pre-line" }}>Aksnqwdnj</p>
+    </section>
+  );
+}
